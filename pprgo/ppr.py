2e207bda81ade60cb48ef6ec226e46dc37649a54
Ariel Ramos Vela <ariel.ramosvela@student.manchester.ac.uk>
2022-06-07 00:51:55 +0200

    S=4

--- pprgo/ppr.py
+++ pprgo/ppr.py
@@ -102,7 +102,10 @@ def calc_ppr_topk_parallel(indptr, indices, deg, alpha, epsilon, nodes, topk):
         y = val_np[idx_y]
         y = y[1:]    #ignore largest element (root node)
 
-        kn = get_kn(x, y, 3)
+        kn = get_kn(x, y, 4)
+
+        if i==0:
+            print(val)
 
         if i <5:
             print('k: ', kn)
